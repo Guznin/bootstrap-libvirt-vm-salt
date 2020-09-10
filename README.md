0ead62933c101d2c80ccc3147751be5c726ddf71
Konstantin Guznin <k.guznin@zameke.com>
2020-09-10 12:56:53 +0300

    Add named args

--- README.md
+++ README.md
@@ -2,8 +2,14 @@
 
 Simple script for bootstrapping virtual machine in lbivirt+qemu environment
 
-It's a bootstarp script. First argument is a hostname, second argument is VCPU's cores, last argument is a memory in GB. For example, if you want to create vm named "my_virt_host_name" whith 8 VCPU's and 4 GB RAM:
+First args is hostname,
+--vcup=8 to set vcpu's value
+--mem=10 to set memmory size 10Gb
+--vlan=99 to set vlan for your vm (see my SaltStack repo)
+For example, if you want to create vm named my_virt_host_name whith 8 VCPU's and 4 GB RAM:
 
-./bootstrap_vm.sh my_virt_host_name 8 4
+### Example
+
+./bootstrap_vm.sh my_virt_host_name --vcpu=8 --mem=4 --vlan=99
 
 more information about virt-build you can find at https://libguestfs.org/virt-builder.1.html
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,61 +1,81 @@
 #!/bin/bash
+#It's a simple bootstrap script for libvirt
 
-if [ "$1" = "--help" ] || [ "$1" = "-h" ] || [ "$1" = "?" ]; then
-
-	echo -e "\nIt's a bootstarp script. First args is hostname,\n
-second argument is VCPU's cores, last arg is memory in GB\n
+#Help function for some info
+function help_func()
+{
+echo -e "\nIt's a bootstarp script. First args is hostname,\n
+--vcup=8 to set vcpu's value\n
+--mem=10 to set memmory size 10Gb\n
+--vlan=99 to set vlan for your vm (see my SaltStack repo)\n
 For example, if you want to create vm named my_virt_host_name whith 8 VCPU's and 4 GB RAM:\n
-\e[33m./bootstrap_vm.sh my_virt_host_name 8 4 \033[0m"
-	exit 0
+./bootstrap_vm.sh my_virt_host_name --vcpu=8 --mem=4 --vlan=99 \n"
+exit 0
+}
+
+#Call help function 
+if [ "$1" = "--help" ] || [ "$1" = "-h" ] || [ "$1" = "?" ] || [[ $# -eq 0 ]]; then
+	help_func
 fi
 
 #https://github.com/Guznin/bootstrap-libvirt-vm-salt
 echo -e "\e[96m <====# WELCOME TO BOOTSTRAP! #====> \033[0m\n"
 
-FQDN="$1.domain.ru"
+#Script arguents 
 NAME=$1
-VCPUS=$2
-MEM_IN_GIG=$3
+FQDN="$NAME.domain.ru"
+VCPU=${VCPU:-2}
+MEM=${MEM:-1024} #Defaul value memmory in MB
 DISKSIZE='10G'
-PORTGROUP='internal_vlan' # it's custom setting
-TEMPLATE='centos-7.8'
-IMAGESDIR='/var/lib/libvirt/images'
-QCOW2="${IMAGESDIR}/${NAME}.qcow2"
-MEMORY=$(( 1024*$3 ))
-SALT_URL='https://salt.domain.ru:8811/signer'
-SALT_TOKEN='YOUR SALT TOKEN'
-CMD1=("curl -L https://bootstrap.saltstack.com | sh -s -- -A salt.domain.ru -i ${FQDN} stable 2019.2.5")
-CMD2=("systemctl enable salt-minion")
-CMD3=("curl -k -sS -X POST -H 'SIGN-TOKEN: "${SALT_TOKEN}"' -d '{"\"nodes\"": ["\"${FQDN}\""]}' ${SALT_URL} ")
+VLAN=${VLAN:-1}
+TEMPLATE='centos-7.8' #specify your OS 
+IMAGESDIR='/var/lib/libvirt/images' #default in libvirt
+FORMAT='qcow2' #you can change format for your disk
+OUT_FILE="${IMAGESDIR}/${NAME}.${FORMAT}"
+SALT_URL='https://salt.domain.ru:8811/signer' #SaltStack url for sign minion
+SALT_TOKEN='MY SALT TOKEN' #your SALT token
+CMD1=("curl -L https://bootstrap.saltstack.com | sh -s -- -A salt.domain.ru -i ${FQDN} stable 2019.2.5") #bootstrap minion
+CMD2=("systemctl enable salt-minion") #enable minion
+CMD3=("curl -k -sS -X POST -H 'SIGN-TOKEN: "${SALT_TOKEN}"' -d '{"\"nodes\"": ["\"${FQDN}\""]}' ${SALT_URL} ") #sign minion
+
+#Parse arguments
+while [ $# -gt 0 ]; do
+   if [[ $2 == "--vcpu="* ]] || [[ $3 == "--mem="* ]] || [[ $4 == "--vlan"* ]] ; then
+        declare VCPU="${2/--vcpu=/}"
+        declare MEM=$(( ${3/--mem=/}*1024 ))
+	declare VLAN="${4/--vlan=/}"
+   fi
+  shift
+done
 
 echo -e "\e[91m Make image \033[0m\n"
 
 #Make image
 virt-builder				\
-	${TEMPLATE}			\
+	${TEMPLATE}				\
 	--hostname	${FQDN}		\
-	--output	${QCOW2}	\
-	--format	qcow2		\
-	--timezone	UTC		\
+	--output	${OUT_FILE}	\
+	--format	${FORMAT}	\
+	--timezone	UTC			\
 	--network		    	\
 	--firstboot-command "${CMD1} && ${CMD2} && ${CMD3}"
 
 echo -e "\e[91m Create vm from image\033[0m\n"
 
 #Create vm from image
-virt-install									\
-	--import								\
-	--name ${NAME}								\
-	--vcpus ${VCPUS}							\
-	--ram ${MEMORY}								\
-	--disk path="${IMAGESDIR}/${NAME}.qcow2"				\
-	--cpuset=auto								\
-	--os-type linux								\
-	--autostart								\
-	--noautoconsole								\
-	--nographics								\
-	--os-variant=centos7.0							\
-	--network network=default,model=virtio,portgroup=${PORTGROUP}
+virt-install											\
+	--import											\
+	--name ${NAME}										\
+	--vcpus ${VCPU}										\
+	--ram ${MEM}										\
+	--disk path="${IMAGESDIR}/${NAME}.${FORMAT}"		\
+	--cpuset=auto										\
+	--os-type linux										\
+	--autostart											\
+	--noautoconsole										\
+	--nographics										\
+	--os-variant=centos7.0								\
+	--network network=ovs-network,model=virtio,portgroup=internal_vlan-${VLAN}
 
 
 if [ $? -ne 0 ]; then
